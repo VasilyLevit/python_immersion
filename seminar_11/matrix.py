fe482203668b2fd3b2a393cf4803ed39e5398322
Vasily Levit <vasily.levit@gmail.com>
2023-07-04 21:03:02 +0800

    added tasks of seminar11

--- seminar_11/matrix.py
+++ seminar_11/matrix.py
@@ -1,68 +1,60 @@
-# Решить задачи, которые не успели решить на семинаре.
-# Добавьте ко всем задачам с семинара строки документации и методы вывода
-# информации на печать.
-# Создайте класс Матрица. Добавьте методы для:
-# ○ вывода на печать,
-# ○ сравнения,
-# ○ сложения,
-# ○ *умножения матриц
+''' Домашнее задание
+Добавьте ко всем задачам с семинара строки документации и методы вывода информации на печать.
+Создайте класс Матрица. Добавьте методы для:
+○ вывода на печать,
+○ сравнения,
+○ сложения,
+○ *умножения матриц'''
 
 
 class Matrix:
-    '''Класс матрица с инициализацией списка списков,с переопределенными
-     методами сложения и умножения и строчного вывода.'''
+    '''Класс матрица переопределенными методами печати, сравнения, сложения и умножения.'''
 
-    def __init__(self, list_of_lists: list[list[int]]):
-        '''Инициализация инит с аргументом list[list[int]].'''
-        self.list_of_lists = list_of_lists
-
-    def __str__(self):
-        'Вывод списка списков построчно.'
-        result = ''
-        for row in self.list_of_lists:
-            for elem in row:
-                result += ''.join(f'{elem}\t')
-            result += ''.join('\n')
-        return result
+    def __init__(self, matrix: list[int]):
+        '''Метод принимает параметр - матрица.'''
+        self.matrix = matrix   
 
     def __eq__(self, other):
-        '''Переопределённый метод для сравнения матриц.
-        Матрицы могут быть равны когда равны их длины и каждый элемент.'''
-        return True if self.list_of_lists == other.list_of_lists else False
+        '''Метод сравнения матриц. Равны, когда равны их длины и каждый элемент.'''
+        return True if self.matrix == other.matrix else False
 
     def __add__(self, other):
-        '''Переопределил метод поэлементного сложения матриц.
-        Можно складывать матрицы одинаковой длины строки первой и столбца второй. '''
+        '''Метод поэлементного сложения матриц. '''
         result = []
         row = []
-        for i in range(len(self.list_of_lists)):
-            for j in range(len(self.list_of_lists[0])):
-                row.append(self.list_of_lists[i][j] + other.list_of_lists[i][j])
+        for i in range(len(self.matrix)):
+            for j in range(len(self.matrix[0])):
+                row.append(self.matrix[i][j] + other.matrix[i][j])
             result.append(row)
             row = []
         return Matrix(result)
 
     def __mul__(self, other):
-        '''Переопределенный метод умножения матриц.
-        Можно складывать матрицы одинаковой длины строки первой и столбца второй.'''
-        m = len(self.list_of_lists)
-        n = len(other.list_of_lists)
-        k = len(other.list_of_lists[0])
+        '''Метод умножения матриц.'''
+        m = len(self.matrix)
+        n = len(other.matrix)
+        k = len(other.matrix[0])
         result = [[0 for _ in range(k)] for _ in range(m)]
         for i in range(m):
             for j in range(k):
-                result[i][j] = sum(self.list_of_lists[i][k] * other.list_of_lists[k][j] for k in range(n))
+                result[i][j] = sum(self.matrix[i][k] * other.matrix[k][j] for k in range(n))
         return Matrix(result)
+    
+    def __str__(self):
+        'Метод печати матрицы.'
+        result = ''
+        for row in self.matrix:
+            for elem in row:
+                result += ''.join(f'{elem}\t')
+            result += ''.join('\n')
+        return result
 
 
 if __name__ == '__main__':
-    matrix_1 = Matrix([[3, 3, 3], [3, 3, 3], [3, 3, 3]])
-    matrix_2 = Matrix([[3, 3, 3], [3, 3, 3], [3, 3, 3]])
-    matrix_sum = matrix_1 + matrix_2
-    print(matrix_sum)
-    matrix_mul = matrix_1 * matrix_2
-    print(matrix_mul)
-    if matrix_1 == matrix_2:
-        print('True')
-    else:
-        print('False')
+    m_1 = Matrix([[1, 2, 3], [4, 5, 6], [7, 8, 9]])
+    m_2 = Matrix([[2, 2, 2], [2, 2, 2], [2, 2, 2]])    
+    m_3 = m_1 + m_2    
+    m_4 = m_1 * m_2
+    print(m_3)
+    print(m_4)
+    print('True') if m_1 == m_2 else print('False')
--- seminar_11/my_string.py
+++ seminar_11/my_string.py
@@ -1,33 +1,32 @@
-# Создайте класс Моя Строка, где:
-# будут доступны все возможности str
-# дополнительно хранятся имя автора строки и время создания
-# (time.time)
+''' Задача 1
+Создайте класс Моя Строка, где:
+будут доступны все возможности str
+дополнительно хранятся имя автора строки и время создания (time.time)'''
 
 # Добавьте к задачам 1 и 2 строки документации для классов.
-import time
+from time import time
 
 
 class MyString(str):
-    '''Расширяемый класс str.'''
+    '''Класс str дополнительно сохраняющий имя автора строки и время создания.'''
 
-    def __new__(cls, value: str, name: str):
+    def __new__(cls, note: str, autor: str):
         '''Расширяеи метод new параметрами value и name.'''
-        instance = super().__new__(cls, value)
-        instance.name = name
-        instance.created_at = time.time()
+        instance = super().__new__(cls, note)        
+        instance.autor = autor
+        instance.creat_time = time()
         return instance
 
     def __str__(self):
-        '''Переопределенный дандер метод строчного выведения экземпляра класса'''
-        return self + " " + f'{self.name} {self.created_at}'
+        '''Переопределенние строчного выведения экземпляра класса.'''
+        return self + " " + f'{self.autor} {self.creat_time}'
 
 
 if __name__ == '__main__':
-    mystr = MyString('САМА строка', 'доп. параметр')
-    # print(mystr.name)
-    # print(mystr.created_at)
+    mystr = MyString('Запись_1', 'Иванов')
+    # print(mystr.autor)
+    # print(mystr.creat_time)
     print(mystr)
     # print(mystr.upper())
-    # help(mystr)
-    # help(MyString)
+    help(MyString)
 
